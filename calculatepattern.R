009bebcc5e13c2e631be2e9906a656fe2183d6ce
Gaurav Sablok <sablokg@gmail.com>
2023-08-23 23:53:47 +0530

    Create calculatepattern.R

---
+++ calculatepattern.R
@@ -0,0 +1,23 @@
+calculatepattern <- function(file_fasta, type_pattern)
+    # a function to calculate certain patterns in long reads
+    # using the stringr package. This function can be run in 
+    # parallel using the library(parallel) in R. It gives the 
+    # count of the occurences which can be easily plotted in R
+    # to use calculatestretch(file_fasta = 
+    # "./test_check.fasta", type_pattern = "TTTTTT")
+    {
+    fasta = readDNAStringSet(file_fasta)
+    pattern = as.character(type_pattern)
+    store_fasta <- vector(length = length(fasta))
+    for (i in seq_along(fasta))
+    {
+        store_fasta[i] <- as.character(fasta[i])
+    }
+    seq_count <- vector(length = length(fasta))
+    for (i in seq_along(store_fasta))
+    {
+        seq_count[i] <- str_count(store_fasta[i], pattern)
+    }
+    return(seq_count)
+
+}
